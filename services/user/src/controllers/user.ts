f754bae8930988bae606122bc16fd94900c75edf
unknown <jeremie229242@gmail.com>
2026-06-15 13:29:51 +0100

    04

--- services/user/src/controllers/user.ts
+++ services/user/src/controllers/user.ts
@@ -2,6 +2,8 @@
 import User from "../model/User.js";
 import jwt from "jsonwebtoken";
 import TryCatch from "../utils/TryCatch.js";
+import axios from "axios";
+import { AuthenticatedRequest } from "../middleware/isAuth.js";
 
 
 
@@ -47,3 +49,94 @@ export const loginUser = TryCatch(async (req, res) => {
       user,
     });
   });
+
+  export const myProfile = TryCatch(async (req: AuthenticatedRequest, res) => {
+    const user = req.user;
+  
+    res.json(user);
+  });
+
+
+  export const getUserProfile = TryCatch(async (req, res) => {
+    const user = await User.findById(req.params.id);
+  
+    if (!user) {
+      res.status(404).json({
+        message: "No user with this id",
+      });
+      return;
+    }
+  
+    res.json(user);
+  });
+
+
+
+  export const updateUser = TryCatch(async (req: AuthenticatedRequest, res) => {
+    const { name, instagram, facebook, linkedin, bio } = req.body;
+  
+    const user = await User.findByIdAndUpdate(
+      req.user?._id,
+      {
+        name,
+        instagram,
+        facebook,
+        linkedin,
+        bio,
+      },
+      { new: true }
+    );
+  
+    const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
+      expiresIn: "5d",
+    });
+  
+    res.json({
+      message: "User Updated",
+      token,
+      user,
+    });
+  });
+  
+  export const updateProfilePic = TryCatch(
+    async (req: AuthenticatedRequest, res) => {
+      const file = req.file;
+  
+      if (!file) {
+        res.status(400).json({
+          message: "No file to upload",
+        });
+        return;
+      }
+  
+      const fileBuffer = getBuffer(file);
+  
+      if (!fileBuffer || !fileBuffer.content) {
+        res.status(400).json({
+          message: "Failed to generate buffer",
+        });
+        return;
+      }
+      const cloud = await cloudinary.uploader.upload(fileBuffer.content, {
+        folder: "blogs",
+      });
+  
+      const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+          image: cloud.secure_url,
+        },
+        { new: true }
+      );
+  
+      const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
+        expiresIn: "5d",
+      });
+  
+      res.json({
+        message: "User Profile pic updated",
+        token,
+        user,
+      });
+    }
+  );
